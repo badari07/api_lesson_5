2d8cada86f38ab81cab0131f34573119bdc68b82
badari07 <badari0793@gmail.com>
2020-05-19 23:37:52 +0530

    feat; singnup jwt

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,25 +1,37 @@
-import config from '../config'
-import { User } from '../resources/user/user.model'
-import jwt from 'jsonwebtoken'
+import config from "../config";
+import { User } from "../resources/user/user.model";
+import jwt from "jsonwebtoken";
 
-export const newToken = user => {
+export const newToken = (user) => {
   return jwt.sign({ id: user.id }, config.secrets.jwt, {
-    expiresIn: config.secrets.jwtExp
-  })
-}
+    expiresIn: config.secrets.jwtExp,
+  });
+};
 
-export const verifyToken = token =>
+export const verifyToken = (token) =>
   new Promise((resolve, reject) => {
     jwt.verify(token, config.secrets.jwt, (err, payload) => {
-      if (err) return reject(err)
-      resolve(payload)
-    })
-  })
+      if (err) return reject(err);
+      resolve(payload);
+    });
+  });
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: "email & password is required" });
+  }
+  try {
+    const user = await User.create(req.body);
+    const token = newToken(user);
+    return res.status(201).send({ token });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).end();
+  }
+};
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {};
 
 export const protect = async (req, res, next) => {
-  next()
-}
+  next();
+};
